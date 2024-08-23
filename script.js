820f614d00d6df1583f2525203a0620f829e48bd
logic lly <ellybachubila@gmail.com>
2024-08-23 17:21:04 +0300

    Got rid of TypeitJS

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// Array of quotes
-const quotes = [
-  "Embark on your software development journey with a trusted ally",
-  "Where dreamers become developers, and creators shape the future",
-  "Visionaries, architects of tomorrow, artisans of code",
-  "Empowering your journey through the digital landscape",
-];
-
-// Function to initialize TypeIt
-function initTypeIt() {
-  new TypeIt("#quote", {
-    strings: quotes,
-    speed: 70,
-    waitUntilVisible: true,
-    loop: false,
-    breakLines: false,
-    nextStringDelay: [2000, 1000],
-  }).go();
-}
-
-// Initialize TypeIt when the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", initTypeIt);
-
 document.addEventListener("DOMContentLoaded", function () {
   var menu = document.getElementById("menu");
   var toggleButton = document.getElementById("menuButton");
